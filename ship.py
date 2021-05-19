b4519900972a9c4f8a1920babcd3bcc5e7c563d4
cliffmieser <cliffmieser@gmail.com>
2021-05-19 14:55:14 -0400

    syntax fixes

--- ship.py
+++ ship.py
@@ -6,7 +6,7 @@ class Ship:
     def __init__(self, ai_game):
         """Initialize the ship and set its starting position."""
         self.screen = ai_game.screen
-        self.screen_react = ai_game.screen.get_rect()
+        self.screen_rect = ai_game.screen.get_rect()
 
 
         #load the ship image and get its rect
