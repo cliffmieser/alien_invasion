0f6c82bc917735d838bc1f5f8079a19a5e60a1e8
cliffmieser <cliffmieser@gmail.com>
2021-05-19 16:10:27 -0400

    syntax changes in settings.py

--- settings.py
+++ settings.py
@@ -6,7 +6,8 @@ class Settings:
     def __init__(self):
         """Initialize the game's settings."""
         #Screen settings
-        #orginal width: 1200, original height: 800
+
+        #orginal width: 1200,  original height: 800
         self.screen_width  = 1000
         self.screen_height = 600
         self.bg_color = (230, 230, 230)
